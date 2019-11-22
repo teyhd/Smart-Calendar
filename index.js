7d66cc1f3c2b183ceaeacd0de16600aa2e4f7dfe
Vlad <vladteyhd@mail.ru>
2019-11-23 02:00:43 +0400

    Исправлен файл вывода расписания

--- index.js
+++ index.js
@@ -121,8 +121,8 @@ function normal_alarm(min) {
     console.log('Вставать в '+alarm_time);
 } //Отнять минуты от времени
 function alarm_time_u(){
-    request.post({url:'https://teyhd.ru/cloud/data/User/admin/home/test.php', form: {q:'88'}}, function(err,httpResponse,body){
-    //request.post({url:'http://localhost/test.php', form: {q:'88'}}, function(err,httpResponse,body){
+    //request.post({url:'https://teyhd.ru/cloud/data/User/admin/home/test.php', form: {q:'88'}}, function(err,httpResponse,body){
+    request.post({url:'http://192.168.0.103/test.php', form: {q:'88'}}, function(err,httpResponse,body){
         if(body!=undefined){
             var obj = JSON.parse(body);
             //timetable
